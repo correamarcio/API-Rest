fa691f48fe4aa71f40309bbe46629033c1096e45
Márcio Corrêa <msc@outlook.com.br>
2020-05-22 00:23:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require("express")
 const app = new express()
 const bodyParser = require("body-parser")
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const JWTSecret = "esteéumtokenaleatorio"
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json())
+
+function auth(req, res, next) {
+    const authtoken = req.headers['authorization']
+    if (authtoken != undefined) {
+        const bearer = authtoken.split(" ")
+        let token = bearer[1]
+
+        jwt.verify(token, JWTSecret, (error, data) => {
+            if (error) {
+                res.status(401)
+                res.json({
+                    token: 'invalido'
+                })
+            } else {
+                req.token = token
+                console.log(data)
+                req.loggedUser = {
+                    id: data.id,
+                    email: data.email
+                }
+                next()
+            }
+        })
+
+        next()
+
+    } else {
+        res.status(401)
+        res.json({
+            token: 'invalido'
+        })
+    }
+}
 
 var DB = {
     times: [{
@@ -32,15 +71,32 @@ var DB = {
             points: 6,
             goals: 8
         },
-    ]
+    ],
+    users: [{
+        id: 1,
+        name: "Marcio",
+        email: "marcio@programador.com.br",
+        password: "learn"
+    }, {
+        id: 2,
+        name: "José",
+        email: "jose@programador.com.br",
+        password: "node"
+    }]
 }
 
-app.get("/times", (req, res) => {
+app.get("/times", auth, (req, res) => {
+
     res.statusCode = 200
-    res.json(DB.times)
+    res.json({
+        user: req.loggedUser,
+        times: DB.times
+    })
 })
 
-app.get("/time/:id", (req, res) => {
+
+
+app.get("/time/:id", auth, (req, res) => {
     let id = req.params.id
     if (isNaN(id) || id == undefined) {
         res.sendStatus(400)
@@ -56,7 +112,7 @@ app.get("/time/:id", (req, res) => {
     }
 })
 
-app.post("/time", (req, res) => {
+app.post("/time", auth, (req, res) => {
     let {
         name,
         points,
@@ -67,8 +123,9 @@ app.post("/time", (req, res) => {
         res.sendStatus(404)
     }
     var time = DB.times.find(time => time.id == id)
+
     if (time != undefined) {
-        res.sendStatus(409)
+        return res.sendStatus(409)
     }
     DB.times.push({
         id,
@@ -79,7 +136,7 @@ app.post("/time", (req, res) => {
     res.sendStatus(200)
 })
 
-app.delete("/time/:id", (req, res) => {
+app.delete("/time/:id", auth, (req, res) => {
     let id = req.params.id
     if (isNaN(id) || id == undefined) {
         res.sendStatus(400)
@@ -96,7 +153,7 @@ app.delete("/time/:id", (req, res) => {
     }
 })
 
-app.put("/time/:id", (req, res) => {
+app.put("/time/:id", auth, (req, res) => {
     let id = req.params.id
     if (isNaN(id) || id == undefined) {
         res.sendStatus(400)
@@ -134,5 +191,53 @@ app.put("/time/:id", (req, res) => {
     }
 })
 
+app.post("/auth", (req, res) => {
+    var {
+        email,
+        password
+    } = req.body
+    if (email == undefined || !(isNaN(email))) {
+        res.status(400)
+        res.json({
+            error: "O email enviado é invalido!"
+        })
+    } else {
+        let user = DB.users.find(u => u.email == email)
+
+        if (user == undefined) {
+            res.status(404)
+            res.json({
+                error: "O email enviado não existe na base de dados!"
+            })
+        } else {
+            if (user.password == password) {
+
+                jwt.sign({
+                    id: user.id,
+                    email: user.id
+                }, JWTSecret, {
+                    expiresIn: '48h'
+                }, (err, token) => {
+                    if (err) {
+                        res.status(400)
+                        res.json({
+                            err: "Falha interna"
+                        })
+                    } else {
+                        res.status(200)
+                        res.json({
+                            token: token
+                        })
+                    }
+                })
+            } else {
+                res.status(401)
+                res.json({
+                    error: "Dados invalidos"
+                })
+            }
+        }
+    }
+})
 
 app.listen("8080", () => {})
